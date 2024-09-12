d3415483178205a7eba40d865bf1412dd6217418
Faqihza Mukhlish - Macbook <faqihza.m@gmail.com>
2024-09-12 22:24:23 +0700

    [update] episode 15

---
+++ Episode_15_Operator_Komparasi/script.js
@@ -0,0 +1,30 @@
+// operator komparasi
+// == equality
+// != inequality
+// === strict equality
+// !== strict inequality
+// > lebih besar dari
+// >= lebih besar sama dengan
+// < lebih kecil dari
+// <= lebih kecil sama dengan
+
+console.log(`10 > 7 = ${10 > 7}`);
+let dataBool = (10 > 7);
+console.log(`10 > 7 = ${dataBool}`);
+
+console.log(`10 >= 7 = ${10 >= 10}`);
+console.log(`7 < 10 = ${7 < 10}`);
+console.log(`7 <= 7 = ${7 <= 7}`);
+
+// persamaan
+console.log(`3 == "3" = ${3 == "3"}`);
+console.log(`3 != 3 = ${3 != "3"}`);
+
+console.log(`3 === 3 = ${3 === 3}`);
+console.log(`3 === "3" = ${3 === "3"}`);
+console.log(`"3" === "3" = ${"3" === "3"}`);
+
+console.log(`3 !== 3 = ${3 !== 3}`);
+console.log(`3 !== "3" = ${3 !== "3"}`);
+console.log(`"3" !== "3" = ${"3" !== "3"}`);
+
